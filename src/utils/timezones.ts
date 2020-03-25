3f33ca84ed80ef27e74ab0dde2a3e56562135b6d
Edwin Hoogerbeets <ehoogerbeets@box.com>
2020-03-25 16:28:47 -0700

    feat(i18n): address Jee's comments
    - Added findTimeZone to get a particular zone for a given box id
    - By default, it only returns the enabled zones now. If you want
    all zones anyways, you can give the option "includeZones": "all"
    - more unit tests

--- src/utils/timezones.ts
+++ src/utils/timezones.ts
@@ -5,10 +5,13 @@
 
 import data, { TimeZone, TimeZoneList } from 'box-locale-data';
 
+export { TimeZone };
+
 const { timezones: boxzones } = data;
 
 export interface TimeZoneOptions {
     tzData?: TimeZoneList;
+    includeZones?: 'all' | 'enabled';
 }
 
 export interface TimeZoneEntries {
@@ -18,6 +21,8 @@ export interface TimeZoneEntries {
 function timezones(options: TimeZoneOptions = {}): TimeZoneEntries {
     const zones: TimeZoneEntries = {};
 
+    const { includeZones = 'enabled' }: { includeZones?: string } = options;
+
     // used the passed in info first. If it is not there, fall back to
     // the one we loaded above from the 'box-locale-data' alias
     let { tzData }: { tzData?: TimeZoneList } = options;
@@ -26,10 +31,20 @@ function timezones(options: TimeZoneOptions = {}): TimeZoneEntries {
     }
 
     tzData.forEach(zone => {
-        zones[String(zone.id)] = zone;
+        if (includeZones === 'all' || zone.enabled) {
+            zones[String(zone.id)] = zone;
+        }
     });
 
     return zones;
+}
+
+export function findTimeZone(id: string | number, options: TimeZoneOptions = {}): TimeZone {
+    const stringId = String(id);
+    return timezones({
+        ...options,
+        includeZones: 'all',
+    })[stringId];
 }
 
 export default timezones;
